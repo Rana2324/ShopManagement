e9e09bfe57430570c1ad7f68fa243ce1a5d75c75
Rana2324 <shohelranatonmoy@gmail.com>
2021-08-27 13:21:47 +0900

    create the component Home,Product,Service,Dashbord,Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashbordComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
